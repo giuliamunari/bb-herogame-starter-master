fc8aaafeb71ca7757c628c17690ca2eb95d213d0
Astrid88 <giu.munari@gmail.com>
2019-06-01 08:56:44 +0200

    created object and declared functions

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,24 @@
 // Write your JS here
+const hero = {
+    name: 'my hero',
+    heroic: true,
+    inventory: [],
+    health: 10,
+    weapon: {
+        type: 'sword',
+        damage: 2
+    }
+}
+function rest(object){
+    if (object.health ===10){
+        alert('You are already healthy')
+    }
+    object.health = 10
+    return object;
+}
+function pickUpItem(){
+
+}
+function equipWeapon(){
+
+}
