408750aef9dde007cac903dc55e7542ad31fb25a
Astrid88 <giu.munari@gmail.com>
2019-06-01 17:29:58 +0200

    cleaned a bit

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -10,10 +10,10 @@ const hero = {
     }
 }
 // --------- RESTING
-let healthOfHero = hero.health
+
 // rest the hero
-function rest(object){
-    if (object.health ===10){
+function rest(object) {
+    if (object.health === 10) {
         alert('You are already healthy')
     }
     object.health = 10
@@ -25,8 +25,8 @@ const potion = document.getElementById('inn')
 
 potion.onclick = () => {
     rest(hero)
-    displayStats(healthOfHero)
-   
+    displayStats(hero.health)
+
 }
 // --------- PICKING UP ITEMS
 
@@ -44,25 +44,25 @@ daggerImg.onclick = () => {
     pickUpItem(hero, dagger)
 }
 // this is the action of picking up the weapon
-function pickUpItem(currentHero, currentWeapon){ 
+function pickUpItem(currentHero, currentWeapon) {
     currentHero.inventory.push(currentWeapon)
-    
+
 }
 
 // --------- EQUIP WEAPON
 const bag = document.getElementById('bag')
 bag.onclick = () => {
     hero.weapon = hero.inventory[0]
-    
+
 }
-function equipWeapon(currentHero){
+function equipWeapon(currentHero) {
     // check if there are items in the bag so we are sure there are default values
-    if (currentHero.inventory[0] !== undefined){
-        
+    if (currentHero.inventory[0] !== undefined) {
+
         currentHero.inventory[0] = currentHero.weapon
         return currentHero.inventory[0]
     }
-    
+
 }
 //------- implementing fighting an enemy
 const enemy = {
@@ -75,82 +75,79 @@ const enemyImage = document.getElementById('enemy')
 // reduce hero health
 
 enemyImage.onclick = () => {
-    
+
     hero.health -= enemy.damage
     enemy.health -= hero.weapon.damage
-    
+
     displayStats(hero.health)
     displayEnemy(enemy.health)
-    if(enemy.health === 0){
+    if (enemy.health === 0) {
         //console.log('win')
         win()
     }
-    if(hero.health <= 0){
+    if (hero.health <= 0) {
         //console.log('died')
         gameOver()
     }
-    
+
 }
 
-function displayStats(health){
+function displayStats(health) {
     const containerStatus = document.getElementById('displayStats')
-    let heroName = hero.name
+    
     const inputField = document.getElementById('newName')
-    if (inputField.value){
-        heroName = inputField.value
+    // removes text from the input
+    if (inputField.value) {
+        hero.name = inputField.value
         inputField.value = ''
     }
-    containerStatus.innerHTML = 
-    `Name: ${heroName},
+
+    containerStatus.innerHTML =
+    `Name: ${hero.name},
     <br>
     Health: ${health},
     <br>
     Weapon: ${hero.weapon.type},
     <br>
     Damage: ${hero.weapon.damage},`
-    
-  
-   
 }
 
 const status = document.getElementById('status')
-function displayEnemy(enemyHealth){
-    
+
+function displayEnemy(enemyHealth) {
     status.innerHTML = `Name: ${enemy.name}, Health: ${enemyHealth}`
-    
 }
-function newImage(imgUrl, buttonReload){
+
+function newImage(imgUrl, buttonReload) {
     const mainContainer = document.getElementById('main')
     //create an image
     const imageEnd = document.createElement('img')
     mainContainer.innerHTML = ''
-    
     imageEnd.src = imgUrl
     // create a button
     const button = document.createElement('button')
     button.classList.add('buttonRestart')
-    button.onclick = () => {
-        location.reload()
-    } 
-    button.innerHTML = buttonReload
-    mainContainer.appendChild(button)
+    button.onclick = () => location.reload() // it reloads the page
+    button.innerHTML = buttonReload //adds text to the button
+    // appends children
+    mainContainer.appendChild(button) 
     mainContainer.appendChild(imageEnd)
-    
-    
 }
-function gameOver(){
+
+function gameOver() {
     console.log('died')
-   const imageOver = 'https://cdn.weasyl.com/static/media/b3/a0/55/b3a055bf9382268a362d5d408a83268e55257be11b1f3216182c00cfddb5bb69.gif'
+    const imageOver = 'https://cdn.weasyl.com/static/media/b3/a0/55/b3a055bf9382268a362d5d408a83268e55257be11b1f3216182c00cfddb5bb69.gif'
     const text = 'Try again'
-   newImage(imageOver, text)
+    newImage(imageOver, text)
 }
-function win(){
+
+function win() {
     console.log('win')
     const imgWin = 'https://media.giphy.com/media/l49JCSwMXyxHnYJws/giphy.gif'
     const text = 'Congratulations! start a new game'
     newImage(imgWin, text)
-    
+
 }
 
-displayStats(healthOfHero)
+displayStats(hero.health)
 displayEnemy(enemy.health)
