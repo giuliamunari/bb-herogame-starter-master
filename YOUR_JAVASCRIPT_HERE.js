7c5000b11c5fb118a7ed241eee871ea43d87c180
Astrid88 <giu.munari@gmail.com>
2019-06-01 10:37:46 +0200

    added some more logic

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -9,11 +9,9 @@ const hero = {
         damage: 2
     }
 }
-const potion = document.getElementById('inn')
+// --------- RESTING
 
-potion.onclick = () => {
-    hero.health = 10
-}
+// rest the hero
 function rest(object){
     if (object.health ===10){
         alert('You are already healthy')
@@ -21,10 +19,41 @@ function rest(object){
     object.health = 10
     return object
 }
-function pickUpItem(currentHero, currentWeapon){
+
+// get potion image and add behaviour
+const potion = document.getElementById('inn')
+
+potion.onclick = () => {
+    rest(hero)
+}
+// --------- PICKING UP ITEMS
+
+// pick up the weapon
+
+// select the weapon
+const daggerImg = document.getElementById('dagger')
+// collect the weapon on click with her properties
+const dagger = {
+    type: 'dagger',
+    damage: 2
+}
+
+daggerImg.onclick = () => {
+    pickUpItem(hero, dagger)
+}
+// this is the action of picking up the weapon
+function pickUpItem(currentHero, currentWeapon){ 
     currentHero.inventory.push(currentWeapon)
-
+    console.log(currentHero.inventory)
 }
-function equipWeapon(){
-
+//console.log(hero.inventory);
+// --------- EQUIP WEAPON
+function equipWeapon(currentHero){
+    currentHero.weapon.damage = 5
+    let current = currentHero.inventory[0]
+    if (current !== currentHero.weapon){
+        current = currentHero.weapon
+    }
+    
 }
+//equipWeapon(hero)
